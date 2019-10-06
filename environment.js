2bd9d84f2a1551557c98c69dd79d3203770e4f0e
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-05 17:14:15 -0700

    remove frontend

--- environment.js
+++
@@ -1,7 +0,0 @@
-//replace with your express server IP Address
-const ip = '192.168.10.78'
-
-const apiUrl = `http://${ip}:3000`
-const sockethost = `ws://${ip}:3001`
-
-module.exports = { apiUrl, sockethost }
